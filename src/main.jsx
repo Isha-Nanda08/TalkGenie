620f42fc8a74f71041127ef6ad5b8fa0d60100dd
Isha-Nanda08 <143190568+Isha-Nanda08@users.noreply.github.com>
2024-08-11 14:05:40 +0530

    navbar

--- src/main.jsx
+++ src/main.jsx
@@ -3,24 +3,37 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Homepage from '../routes/homepage/Homepage.jsx'
+import Homepage from './routes/homepage/Homepage.jsx'
 import Dashboard from './routes/dashboard/Dashboard.jsx'
 import Chatpage from './routes/chatpage/Chatpage.jsx'
+import RootLayout from './layouts/rootLayout/RootLayout.jsx'
+import DashboardLayout from './layouts/dashboardLayout/DashboardLayout.jsx'
 
 
 const router=createBrowserRouter([
   {
-  path:"/",
-  element:(
-    <Homepage/>
-  ),
-},
-  {
-    path:"/dashboard",
-    
-      children: [ { path :"dashboard",element:<Dashboard/>,},
-        {path:"/dashboard/chat/:id" ,element:<Chatpage/>}], 
-  },
+    element:<RootLayout/>,
+    children:[
+      {
+        path:"/",element:<Homepage/>
+      },
+      {
+        element:<DashboardLayout/>,
+        children:[
+          {
+            path:"/dashboard",
+            element:<Dashboard/>
+          },
+          {
+            path:"/dashboard/chat/:id",
+            element:<Chatpage/>
+          }
+        ]
+      }
+
+    ]
+
+  }
 ]);
 
 
