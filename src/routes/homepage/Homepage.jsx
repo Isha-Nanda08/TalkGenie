3b0af89f9f187a9c8fa3e261cf80250bef82812c
Isha-Nanda08 <143190568+Isha-Nanda08@users.noreply.github.com>
2024-08-12 17:56:48 +0530

    media

--- src/routes/homepage/Homepage.jsx
+++ src/routes/homepage/Homepage.jsx
@@ -1,6 +1,12 @@
 import { Link } from 'react-router-dom'
 import './homepage.css'
+import { TypeAnimation } from 'react-type-animation'
+import { useState } from 'react';
 const Homepage = () => {
+
+  const [typingStatus,setTypingStatus]=useState("human1");
+
+
   return (
     <div className="homepage">
       <img src='/orbital.png' alt="" className='orbital'/>
@@ -21,6 +27,41 @@ const Homepage = () => {
             <div className="bg"></div>
           </div>
           <img src='/bot.png' alt='' className='bot'/>
+          <div className="chat">
+            <img src={typingStatus==="human1" ? "/human1.jpeg" : typingStatus==="human2" ? "/human2.jpeg" : "bot.png"} alt="" />
+            <TypeAnimation
+              sequence={[
+                'Human1:We produce food for Mice',
+                2000, ()=>{
+                    setTypingStatus("bot")
+                },
+                'Bot:We produce food for Hamsters',
+                2000, ()=>{
+                  setTypingStatus("human2")
+                },
+                'Human2:We produce food for Guinea Pigs',
+                2000, ()=>{
+                  setTypingStatus("bot")
+                },
+                'Bot:We produce food for Chinchillas',
+                2000,()=>{
+                  setTypingStatus("human1")
+                }
+              ]}
+              wrapper="span"
+              repeat={Infinity}
+              cursor={true}
+              omitDeletionAnimation={true}
+            />
+          </div>
+        </div>
+      </div>
+      <div className="terms">
+        <img src="/logo.png" alt="" />
+        <div className="links">
+          <Link to="/">Terms of Service</Link>
+          <span>|</span>
+          <Link to ="/">Privacy Policy</Link>
         </div>
       </div>
 
